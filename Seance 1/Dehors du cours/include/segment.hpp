d2a5e4133473746798c0a520522c552cfe815a78
sergio-contente <sergio.mcontente@gmail.com>
2023-09-07 16:44:47 +0200

    ✨ Lol

--- Seance 1/Dehors du cours/include/segment.hpp
+++ Seance 1/Dehors du cours/include/segment.hpp
@@ -1,1 +1,21 @@
+#ifndef __SEGMENT_H__
+#define __SEGMENT_H__
 
+#include <iostream>
+#include "point.hpp"
+
+struct Segment
+{
+	private:
+		Point m_p1, m_p2;
+		int m_slope, m_intercept;
+	public:
+		Segment(const Point& point1, const Point& point2) : m_p1(point1), m_p2(point2) {
+			std::cout << "Segment between points (" << m_p1.getX() << ", " << m_p1.getY() << ")\n" <<
+			", ("  
+		}
+};
+
+
+
+#endif // !"__SEGMENT_H"
