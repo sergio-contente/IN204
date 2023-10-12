eefbc44c422f80fdb90e42894e79795074b3afb9
sergio-contente <sergio.mcontente@gmail.com>
2023-10-12 11:48:01 +0200

    🚀  TD 6 finished

--- Seance 6/include/simple_sort.hpp
+++ Seance 6/include/simple_sort.hpp
@@ -4,7 +4,9 @@
 
 template<typename  iterator>
 void simple_sort(iterator start, iterator end)
+  requires(std::forward_iterator<iterator> && ! std::random_access_iterator<iterator> && std::input_or_output_iterator<iterator>)
 {
+		std::cout << "Insertion Sort\n";
     for(;start != end; start ++)
     {
         auto it = start; it++;
@@ -17,8 +19,23 @@ void simple_sort(iterator start, iterator end)
     }
 }
 
+template<typename iterator>
+void simple_sort(iterator start, iterator end) 
+    requires(std::random_access_iterator<iterator> && std::input_or_output_iterator<iterator>)
+{
+    std::cout << "Quicksort\n";
+    return std::sort(start, end);
+}
+
+template<typename T>
+concept Browsable = requires (const T& a)
+{
+    { a.begin() } -> std::forward_iterator;
+    { a.end() } -> std::forward_iterator;
+};
 
 template<typename  containerT, typename charT, typename traits = std::char_traits<charT>>
+requires Browsable<containerT>
 std::basic_ostream<charT, traits>& operator << (std::basic_ostream<charT, traits>& aStream, const containerT& aContainer)
 {
     aStream << "{";
@@ -31,3 +48,11 @@ std::basic_ostream<charT, traits>& operator << (std::basic_ostream<charT, traits
     }
     aStream << "}";
 } 
+
+
+template<typename T, typename charT, typename traits = std::char_traits<charT>>
+std::basic_ostream<charT, traits>& operator << (std::basic_ostream<charT, traits>& aStream, const T& aContainer)
+{
+    std::cout << "NONE";
+    return aStream;
+}
