4dbd163a47b7c631db4ba58d92294c35c9ae1e2c
sergio-contente <sergio.mcontente@gmail.com>
2023-10-12 11:06:59 +0200

    🚨 CMAKE GENERATING SEG FAULT ERROR

--- Seance 6/bin/main.cpp
+++ Seance 6/bin/main.cpp
@@ -1,1 +1,15 @@
 #include "../include/simple_sort.hpp"
+
+#include<list>
+
+#include<list>
+ 
+struct Toto {};
+int main() 
+{
+    std::list<int> v = {1, 7, 3, 4, 9, 2, 5};
+    simple_sort(v.begin(), v.end());
+    std::cout << v;
+    std::cout << Toto() << std::endl;
+    return 0;
+}
--- Seance 6/include/simple_sort.hpp
+++ Seance 6/include/simple_sort.hpp
@@ -1,20 +1,33 @@
 #pragma once
 #include <algorithm>
+#include <iostream>
 
-template<typename iterator>
+template<typename  iterator>
 void simple_sort(iterator start, iterator end)
-	requires(std::forward_iterator<iterator> && std::input_or_output_iterator<iterator>)
 {
-	for (; start != end; start++)
-	{
-		auto it = start;
-		it++;
-		for (; it != end; it ++)
-		{
-			// Compare si les deux elements sont dans le bon ordre
-			if (*start > *it)
-				std::swap(*start, *it);
-		}
-	}
-	
+    for(;start != end; start ++)
+    {
+        auto it = start; it++;
+        for(;it != end; it ++)
+        {
+            // Compare si les deux elements sont dans le bon ordre.
+            if (*start > *it)
+                std::swap(*start, *it);
+        }
+    }
 }
+
+
+template<typename  containerT, typename charT, typename traits = std::char_traits<charT>>
+std::basic_ostream<charT, traits>& operator << (std::basic_ostream<charT, traits>& aStream, const containerT& aContainer)
+{
+    aStream << "{";
+    auto end = aContainer.end();
+    for(auto it = aContainer.begin(); it != end;)
+    {
+        aStream << *it ++;
+        if(it != end)
+            aStream << ", ";
+    }
+    aStream << "}";
+} 
